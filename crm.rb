2798b2f8e59b29ee284cbcb54a84f247695c3b75
Daiani <Daianim3@Daianis-MacBook-Air.local>
2015-04-08 15:38:48 -0400

    adicionando rolodex class

--- crm.rb
+++ crm.rb
@@ -1,6 +1,19 @@
+require_relative 'rolodex'
+require_relative 'contact'
 require 'sinatra'
 
 get '/' do
 @crm_app_name = "My CRM"
 erb :index
 end
+
+get '/contacts' do
+	@contacts = []
+	@contacts << Contact.new("Yehuda", "Katz", "yeduda@gmail.com", "Developer")
+	@contacts << Contact.new("Sarah", "Andrade", "mark@gmail.com", "CFO")
+	@contacts << Contact.new("Paul", "Macedo", "paul@gmail.com", "sales")
+
+erb :contacts
+end
+
+
