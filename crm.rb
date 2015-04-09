7ac07d7128954ea5275c53ef630d809e3f060424
Daiani <Daianim3@Daianis-MacBook-Air.local>
2015-04-09 13:34:46 -0400

    first layout

--- crm.rb
+++ crm.rb
@@ -2,18 +2,27 @@ require_relative 'rolodex'
 require_relative 'contact'
 require 'sinatra'
 
+$rolodex= Rolodex.new
+
 get '/' do
 @crm_app_name = "My CRM"
 erb :index
 end
 
 get '/contacts' do
-	@contacts = []
-	@contacts << Contact.new("Yehuda", "Katz", "yeduda@gmail.com", "Developer")
-	@contacts << Contact.new("Sarah", "Andrade", "mark@gmail.com", "CFO")
-	@contacts << Contact.new("Paul", "Macedo", "paul@gmail.com", "sales")
+	erb :contacts
 
-erb :contacts
+
 end
 
 
+get '/contacts/new' do
+	erb :new_contact
+end
+
+
+post '/contacts' do
+    new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+  $rolodex.add_contact(new_contact)
+  redirect to('/contacts')
+end
