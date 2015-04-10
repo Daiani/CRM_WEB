82893147db1d638c144032d417e0269696f04472
Daiani <Daianim3@Daianis-MacBook-Air.local>
2015-04-10 14:58:38 -0400

    creting contact 1000

--- crm.rb
+++ crm.rb
@@ -2,17 +2,25 @@ require_relative 'rolodex'
 require_relative 'contact'
 require 'sinatra'
 
+
+
+
 $rolodex= Rolodex.new
 
+# Temporary fake data so that we always find contact with id 1000.
+$rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
+
+contact = $rolodex.find(1000)
+
+
+
 get '/' do
-@crm_app_name = "My CRM"
-erb :index
+	@crm_app_name = "My CRM"
+	erb :index
 end
 
 get '/contacts' do
 	erb :contacts
-
-
 end
 
 
@@ -20,9 +28,16 @@ get '/contacts/new' do
 	erb :new_contact
 end
 
+get '/contacts/edit' do
+end
+
+get "/contacts/1000" do
+	@contact = $rolodex.find(1000)
+	erb :show_contact
+end
 
 post '/contacts' do
-    new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
-  $rolodex.add_contact(new_contact)
+  new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+  $contact.add_contact(new_contact)
   redirect to('/contacts')
 end
